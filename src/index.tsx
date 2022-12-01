7e0acbc36b95c4267017f07ad2e186425e0b4335
flyingmt <flyingmt@gmail.com>
2022-12-01 21:56:49 +0900

    2 files modified, 1 added, 1 deleted

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import store from './redux/store';
+import store from './redux';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
